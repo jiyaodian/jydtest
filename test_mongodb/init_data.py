7405fda144600b3a10bb17fbd21e60517af48895
jyd <jiyaodian@gmail.com>
2013-06-21 14:31:55 +0800

    添加初始化数据库脚本

---
+++ test_mongodb/init_data.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#! coding: utf-8
+
+"""
+初始化数据库，添加五千万条数据。每条数据为一个list
+"""
+from pymongo import Connection
+
+coll = Connection("127.0.0.1", 27017)["test"]["test"]
+data = [2934234]*200
+for i in xrange(50000000):
+	dic = {
+		str(i): data,
+	}
+	coll.save(dic)
+
+
+
---
+++ test_mongodb/test_mongo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os
+from pymongo import Connection
+
+conn = Connection("127.0.0.1", 27017)
+
+os.path()
+
+
+
